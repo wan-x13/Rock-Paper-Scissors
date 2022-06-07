cda2be479292d175fb54a9d8f239780f4b1edfd9
David Wanga <davidwangajean@gmail.com>
2022-06-07 22:36:23 +0100

    CREATE: function updateChoice, game Update: function playRound Event : add event in form Element

--- constants.js
+++ constants.js
@@ -1,9 +1,2 @@
-export const form = document.querySelector('form');
-export const input = document.querySelector('input');
-export const userPlayer = document.querySelector(".userPlayer");
-export const currentLogoPlayer = document.querySelector('.currentLogoPlayer');
-export const scorePlayer = document.querySelector('.scorePlayer');
-export const scoreComputer = document.querySelector(".scoreComputer");
-export const currentLogoComputer = document.querySelector(".currentLogoComputer");
-export const card = document.querySelector('.card');
+
 
--- fonction.js
+++ fonction.js
@@ -1,67 +1,145 @@
 
-import { currentLogoComputer , currentLogoPlayer, form, input,scoreComputer,scorePlayer} from './constants';
-import  {userPlayer , card} from './constants';
+
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const userPlayer = document.querySelector(".userPlayer");
+const currentLogoPlayer = document.querySelector('.currentLogoPlayer');
+const scorePlayer = document.querySelector('.scorePlayer');
+const scoreComputer = document.querySelector(".scoreComputer");
+const currentLogoComputer = document.querySelector(".currentLogoComputer");
+const card = document.querySelector('.card');
+const homePage = document.querySelector(".homePage");
+const gamePage = document.querySelector(".game");
+const image = document.querySelectorAll("img");
+const titleScore = document.querySelector(".titleScore > h2")
 
 
 let playerScore = 0;
 let computerScore = 0;
 let response;
 
+form.addEventListener("submit", function(e){
+
+  e.preventDefault();
+  if(input.value.trim() === ""){
+
+    input.classList.add("invalid");
+    input.nextElementSibling.innerText = "cannot be blank";
+  }
+  else if(input.value.length <3){
+    input.classList.add("invalid");
+    input.nextElementSibling.innerText = "no less than 3 characters";
+
+  }
+  else {
+    input.classList.add("valid")
+    input.nextElementSibling.innerText = "";
+    homePage.style.display = "none";
+    gamePage.style.display = "block";
+
+  }
+})
+
+
 
 const computerPlay = function(){
-    let randomNumber = Math.floor(Math.random() *3);
-    randomNumber = randomNumber == 0 ? 'Rock': 
-                   randomNumber == 1 ? 'Paper' : 'Scissor';
-     return randomNumber;
+  let randomNumber = Math.floor(Math.random() *3);
+  randomNumber = randomNumber == 0 ? 'Rock': 
+                 randomNumber == 1 ? 'Paper' : 'Scissor';
+   return randomNumber;
 
 }
-const playRound  = function(playSelection , computerSelection){
-    if(playSelection == computerSelection) return "Null";
-    if(playSelection == 'Paper' && computerSelection == 'Rock'
-    || playSelection == 'Rock' && computerSelection == 'Scissor'
-    || playSelection == 'Scissor' && computerSelection == 'Paper'){
-        return 'player';
-    }
-    else return  ' Computer';
+
+
+let imageAttr;
+
+
+const updateChoice = (imageAttr,computerSelection)=>{
+
+  switch(imageAttr){
+    case 'Rock':
+      currentLogoPlayer.innerText = "✊🏻"
+      break;
+    case 'Paper':
+      currentLogoPlayer.innerText = "✋🏻"
+      break;
+    case 'Scissor':
+      currentLogoPlayer.innerText = "✌🏻"
+      break;
+  }
+
+
+  switch(computerSelection){
+    case 'Rock':
+      currentLogoComputer.innerText = "✊🏻"
+      break;
+    case 'Paper':
+      currentLogoComputer.innerText = "✋🏻"
+      break;
+    case 'Scissor':
+      currentLogoComputer.innerText = "✌🏻"
+      break;
+  }
+
 }
 
-const game = function (){
+const playRound  = function(e){
+
+  imageAttr = e.target.getAttribute('id')
+
+  const computerSelection = computerPlay();
+
+  if(imageAttr == computerSelection) return "It's a Tie";
+  if(imageAttr == 'Paper' && computerSelection == 'Rock'
+  || imageAttr == 'Rock' && computerSelection == 'Scissor'
+  || imageAttr == 'Scissor' && computerSelection == 'Paper'){
+
+     updateChoice(imageAttr, computerSelection);
 
 
-const player= 'Scissor';
-const computerSelection = computerPlay();
+
+      return 'player';
+      
+  }
+  else {
+
+    updateChoice(imageAttr, computerSelection);
+    return  'computer';
+  }
+
+}
+const getWinner = (a,b)=>{
+  if(a==5){
 
 
-  for(let i = 0; i<5 && response !== 'N'; i++){
+  }
+  else{
 
-  // response = prompt('voulez vous jouer ? oui ou non (O/N') ;
+  }
   
 
-    let result = playRound(player , computerSelection);
-    console.log(result);
-    if(result === 'player'){
-  
-      playerScore += 1;
-    }
-    else{
-      computerScore +=1
-    }
-    let winner = getWinner();
-    console.log(winner);
-
-  }
 }
-const getWinner = function(){
 
-  if(computerScore === 5 ){
-
-    return " Computer win the Game"
+const game = function (e){
+  let response = playRound(e);
+  if(response === "player"){
+    playerScore += 1;
+    titleScore.innerText = "Score";
+    scorePlayer.innerText = playerScore
   }
-  else if(playerScore === 5){
+  if(response === "computer"){
+    computerScore += 1;
+    titleScore.innerText = "Score";
+    scoreComputer.innerText= computerScore
+  }
+  if(response === "It's a Tie"){
+    titleScore.innerText = response;
+    titleScore.style.color = "red";
+    titleScore.style.fontSize = "35px"
 
-    return "player win the Game"
   }
 }
 
 
-console.log(game());
+
+image.forEach(img => img.addEventListener('click' , game))
